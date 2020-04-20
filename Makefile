52af4a6b34ee2af414fa4ed7cfc9b8c9bab9f988
slebras <slebras@lbl.gov>
2020-04-20 09:56:57 -0700

    improving testing to include json schema validation for the metadata_validation.yml file

--- Makefile
+++ Makefile
@@ -3,4 +3,4 @@
 
 test:
 	python3 -c "import yaml, sys; yaml.safe_load(sys.stdin)" < sample_uploader_mappings.yml
-	python3 -c "import yaml, sys; yaml.safe_load(sys.stdin)" < metadata_validation.yml
+	python3 validate_schemas.py
---
+++ validate_schemas.py
@@ -0,0 +1,47 @@
+import yaml
+from jsonschema import validate
+
+_META_VAL_JSONSCHEMA = {
+    'type': 'object',
+    'definitions': {
+        'validator_set': {
+            'type': 'object',
+            # validate values only
+            'additionalProperties': {
+                'type': 'object',
+                'properties': {
+                    'key_metadata': {
+                        'type': 'object',
+                        'additionalProperties': {
+                            'type': ['number', 'boolean', 'string', 'null']
+                        }
+                    },
+                    'validators': {
+                        'type': 'array',
+                        'items': {
+                            'type': 'object',
+                            'properties': {
+                                'module': {'type': 'string'},
+                                'callable-builder': {'type': 'string'},
+                                'parameters': {'type': 'object'}
+                            },
+                            'additionalProperties': False,
+                            'required': ['module', 'callable-builder']
+                        }
+
+                    }
+                },
+                'required': ['validators']
+            }
+        },
+        'additionalProperties': False,
+    },
+    'properties': {
+        'validators': {'$ref': '#/definitions/validator_set'},
+        'prefix_validators': {'$ref': '#/definitions/validator_set'},
+    },
+    'additionalProperties': False
+}
+with open("metadata_validation.yml") as f:
+    cfg = yaml.safe_load(f)
+validate(instance=cfg, schema=_META_VAL_JSONSCHEMA)
